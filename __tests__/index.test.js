23a917452668693f33c7678b9495a5a3ff69a8b1
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-02-22 16:49:08 -0800

    update h test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,4 +11,33 @@ describe('h', () => {
 
     expect(node).toEqual(expected)
   })
+
+  it('should create an object with the right props and children', () => {
+    const node = h('div')({ id: 'foo' })(
+      h('span')()('Hello World'),
+      'Sibling Text Node',
+      h('span')()('Another Node')
+    )
+    const expected = {
+      tag: 'div',
+      props: {
+        id: 'foo'
+      },
+      children: [
+        {
+          tag: 'span',
+          props: {},
+          children: ['Hello World']
+        },
+        'Sibling Text Node',
+        {
+          tag: 'span',
+          props: {},
+          children: ['Another Node']
+        }
+      ]
+    }
+
+    expect(node).toEqual(expected)
+  })
 })
