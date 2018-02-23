7354db73e2626031137fee57bd9e46a038d12ae6
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-02-22 16:49:45 -0800

    update config with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: 'src/index.js',
   output: {
@@ -5,6 +7,10 @@ export default {
     format: 'umd',
     name: 'phomo',
     sourcemap: true
-  }
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 }
-
